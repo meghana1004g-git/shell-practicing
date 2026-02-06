af991496253e802060220e8d003b4dd12d7cdaac
meghana1004g-git <meghana1004g@gmail.com>
2026-02-06 17:38:00 +0530

    actions

---
+++ 9.conditions.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+-gt-->grater than
+-lt-->less than
+-eq-->equal to
+-ne-->not equal
+
+
+NUMBER=25
+if [ $NUMBER -gt 20 ]; then
+echo "Given number is: $NUMBER is greater than 25"
+fi
+
