3abfb1c970146fc79fc10995997992d1d34840a6
meghana1004g-git <meghana1004g@gmail.com>
2026-02-06 13:17:15 +0530

    actions

--- 3.variables.sh
+++ 3.variables.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-PERSON1=Suresh #no space b/w value and =
-PERSON2=Ramesh
+PERSON1=Trump #no space b/w value and =
+PERSON2=Putin
 
 echo "$PERSON1":: Hello $PERSON2, how are u?"
 echo "$PERSON2":: Hi $PERSON1, i am good thanks for asking, how u doing"
