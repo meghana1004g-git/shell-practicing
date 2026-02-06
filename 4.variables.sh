5c04b80194024018ac249e1b6e5c508d1b40c1b2
meghana1004g-git <meghana1004g@gmail.com>
2026-02-06 14:12:08 +0530

    actions

--- 4.variables.sh
+++ 4.variables.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-PERSON1=1 #no space b/w value and =
-PERSON2=2
+PERSON1=$1#no space b/w value and =
+PERSON2=$2
 
 echo "$PERSON1":: Hello $PERSON2, how are u?"
 echo "$PERSON2":: Hi $PERSON1, i am good thanks for asking, how u doing"
