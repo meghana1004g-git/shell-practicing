3ea2e0fb7d73327c65b8fdf1c1657cada08d9263
meghana1004g-git <meghana1004g@gmail.com>
2026-02-06 19:39:34 +0530

    actions

---
+++ 11.functions.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+echo "Please run this script with root user access"
+exit 1
+fi
+
+VALIDATE(){
+    if { $1 -ne 0 ]; then
+    echo "$2 .... FAILURE"
+    exit 1
+    else
+    echo "$2 .... SUCCESS"
+    
+    fi
+
+}
+
+dnf install ngnix -y
+VALIDATE $? "Installing nginx"
+
+dnf install mysql -y
+VALIDATE $? "Installing mysql"
+
+dnf install nodejs -y
+VALIDATE $? "INstalling nodejs"
