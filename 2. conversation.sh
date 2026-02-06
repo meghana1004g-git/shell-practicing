72e83b0b9fc99fc76e7acf73fbf8aad9cb353186
meghana1004g-git <meghana1004g@gmail.com>
2026-02-06 12:32:05 +0530

    actions

---
+++ 2. conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "suresh:: Hello Ramesh, how are u?"
+echo "Ramesh:: Hi Suresh, i am good thanks for asking, how u doing"
+echo "Suresh:: doing good, what are u learning Ramesh"
+echo "Ramesh:: i am learning shell-scripting, what about u?"
+echo "Suresh:: i am working."
