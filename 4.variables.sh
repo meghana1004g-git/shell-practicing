88e9a992ffb20c73d4b611fc721efb44d5ab5464
meghana1004g-git <meghana1004g@gmail.com>
2026-02-06 14:08:04 +0530

    actions

---
+++ 4.variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+PERSON1=1 #no space b/w value and =
+PERSON2=2
+
+echo "$PERSON1":: Hello $PERSON2, how are u?"
+echo "$PERSON2":: Hi $PERSON1, i am good thanks for asking, how u doing"
+echo "$PERSON1":: doing good, what are u learning $PERSON2"
+echo "$PERSON2":: i am learning shell-scripting, what about u?"
+
