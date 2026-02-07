5635624ed7319d6c2dbd614485863ae856f17790
meghana1004g-git <meghana1004g@gmail.com>
2026-02-07 12:11:34 +0530

    actions

--- 17.colors.sh
+++ 17.colors.sh
@@ -13,8 +13,8 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
 echo -e $R "Please run this script with root user access $N" | tee -a $LOGS_FILE
-    exit 1
-exit 1
+ exit 1
+
 fi
 mkdir -p $LOGS_FOLDER
 
